01d324f0f618f593f1eaed7812be8bddf3dfa88d
Brandon <brandonrodriguezvega@gmail.com>
2022-10-12 18:36:44 -0400

    saving work

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,22 @@
-var formResults = document.querySelector('');
-var formResults = document.querySelector('');
-var formResults = document.querySelector('');
-var formResults = document.querySelector('');
-var formResults = document.querySelector('');
+var rain = $('#rain');
+var wind = $('#wind');
+var temp = $('#temp');
+
+
+var responseText = document.getElementById('Weather');
+
+var APIKey = 'a8YgP4C0Q8FBzLTMOoG2LGs8GVCIQlwB';
+
+function HourlyWeather() {
+var WeatherCard = 'http://dataservice.accuweather.com/forecasts/v1/hourly/12hour/31520?apikey=a8YgP4C0Q8FBzLTMOoG2LGs8GVCIQlwB';
+
+fetch(WeatherCard)
+.then(function (responce) {
+    return responce.json();
+})
+.then(function (data) {
+    console.log(data);
+})
+}
+
+HourlyWeather()
