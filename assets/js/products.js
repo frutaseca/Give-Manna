99934c63be3a9afc7e5fd2f56116b8cf4c47c7dc
Ava Blake <avablakedesign@protonmail.com>
2022-10-18 19:15:46 -0400

    Comments added

--- assets/js/products.js
+++ assets/js/products.js
@@ -1,18 +1,9 @@
+//These variables hook into the HTML.
 const productsContainer = document.getElementById("products-container")
 const productsSections = productsContainer.children
 const productPrices = document.querySelectorAll(".product-price")
 const productImages = document.querySelectorAll(".product-image") 
-
-/*
-    fetch(url)
-    .then(function {
-        console.log(response)
-    .then (function {
-        let data = response.json
-
-    })    
-    })
-*/
+//This gets the product ID and waits for the information where the code is async.
 async function getProduct(id) {
     let response = await fetch(`https://api.bluecartapi.com/request?api_key=BBC6D0DEE68A4CA9B71D00EA28E05C40&type=product&item_id=${id}`)
     console.log("response", response); 
@@ -23,13 +14,7 @@ async function getProduct(id) {
     let price = data.product.buybox_winner.price;
     return {image, price};
   }
-
-  /*
-    doMath() 1 +1 = 2
-    doMoreMmath() 2 + 2 = 4
-    getProduct => await make a fetch request (three s) =>await wait for response to be JSON (one s) => (do other stuff)
-  */
-
+//This gets the information from local storage and sets the array to empty.
   async function renderProducts() {
     let wonderBread = null;
     let cheese = null;
@@ -43,24 +28,26 @@ async function getProduct(id) {
         let savedProducts = JSON.parse(cachedProducts);
         productsArray = savedProducts.productsArray;
     }
+  //This waits for the product ID's and makes an array based on the product ids. 
     else {  
       wonderBread = await getProduct("37858875");
       cheese = await getProduct("10452377");
-      ham = await getProduct("10315957")
-      pb = await getProduct("10315475")
-      jelly = await getProduct("364924551")
-      sjelly = await getProduct("10315473")
-      productsArray = [wonderBread, cheese, ham, pb, jelly, sjelly]
+      ham = await getProduct("10315957");
+      pb = await getProduct("10315475");
+      jelly = await getProduct("364924551");
+      sjelly = await getProduct("10315473");
+      productsArray = [wonderBread, cheese, ham, pb, jelly, sjelly];
       let productsObj = {
         productsArray : productsArray
       }
       let productsStringFormat = JSON.stringify(productsObj);
-      localStorage.setItem("cachedProducts", productsStringFormat)
+      localStorage.setItem("cachedProducts", productsStringFormat);
     }  
     console.log(productsArray);
-    Array.from(productsSections).forEach((element, i) => {
-        productPrices[i].textContent = `$${productsArray[i].price}`
-        productImages[i].src = productsArray[i].image
+    //This makes images and prices based on the elements in the array.
+    Array.from(productsArray).forEach((element, i) => {
+        productPrices[i].textContent = `$${productsArray[i].price}`;
+        productImages[i].src = productsArray[i].image;
     });
   }
   renderProducts()
